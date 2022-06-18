343cb2daabf65eb7b6656df716beb59ba52f4f65
jcruzpalermo <jcruzpalermo@gmail.com>
2022-06-18 00:39:54 -0300

    lista de elementos

---
+++ entregable2.js
@@ -0,0 +1,15 @@
+const lista = [
+    "Juan cruz",
+    25,
+    true,
+    new Date("july 20 1996"),
+    {
+        titulo: "The Maze Runner",
+        autor: "James Dashner",
+        fecha: new Date("october 6 2009"),
+        URL: "https://www.sev.gob.mx/clasesdesdecasa/documentos/ef419121589180a9e6564ece3d56ef6aMaze-Runner_correr-o-morir.pdf"
+    }
+]
+
+
+
---
+++ iniciacion_tipos.js
@@ -0,0 +1,24 @@
+//Tipos primitivos
+
+//Number
+4
+//string
+"hola mundo";
+'hola mundo';
+
+//booleanos
+true;
+false;
+
+//nulo y undefined
+null;
+undefined;
+
+//null, undefined, false, o ===>Todas son falsy
+console.log(null===undefined) false
+
+if () {
+    console.log("cumple") cumple 
+}else {
+    console.log("no cumple") no cumple;
+}
